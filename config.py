10f60747aea7ebf113521b941cd2075c5e2b37f2
Bruno Silva6 <bruno_silva6@dell.com>
2025-03-18 10:36:03 -0300

    Configured the game window

---
+++ config.py
@@ -0,0 +1,4 @@
+# Game Window settings
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+GRID_SIZE = SCREEN_WIDTH // 2
--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+from turtle import Screen
+from config import(
+    SCREEN_WIDTH, SCREEN_HEIGHT, GRID_SIZE
+)
+
+def setup_game_window():
+    """Setups the game window"""
+    screen = Screen()
+    screen.title("Turtle Crossing!")
+    screen.setup(width = SCREEN_WIDTH, height = SCREEN_HEIGHT)
+    screen.bgcolor("white")
+    screen.tracer(0)
+
+    # Establishes a grid-based coordinate system
+    screen.setworldcoordinates(-GRID_SIZE, -GRID_SIZE, GRID_SIZE, GRID_SIZE)
+
+    return screen
+
 def main():
-    pass
+    # Sets up the game window
+    screen = setup_game_window()
+    
+    # Starts the game loop
+    screen.mainloop()
 
 if __name__ == "__main__":
     main()
