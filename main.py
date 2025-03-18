64249dad11d65829acb3be3ab5c060be6674f883
Bruno Silva6 <bruno_silva6@dell.com>
2025-03-18 10:52:37 -0300

    Drew the player object

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Screen
+from player import Player
 from config import(
     SCREEN_WIDTH, SCREEN_HEIGHT, GRID_SIZE
 )
@@ -16,11 +17,27 @@ def setup_game_window():
 
     return screen
 
+def game_loop(screen, player):
+    """Handles the game loop and updates the screen
+    Args:
+        screen (Screen): The screen object from the Turtle library
+        player (Player): The player object
+    """
+    screen.update()
+
+    # Schedule the next screen update.
+    # For reference, 1000ms = 1 second
+    screen.ontimer(lambda: game_loop(screen, player), 100)
+
 def main():
     # Sets up the game window
     screen = setup_game_window()
+
+    # Initializes the game objects
+    player = Player()
     
     # Starts the game loop
+    game_loop(screen, player)
     screen.mainloop()
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,7 +1,25 @@
 from turtle import Turtle
+from config import (
+    GRID_SIZE,
+    ROW_HEIGHT
+)
 
 class Player(Turtle):
     
     def __init__(self):
         """"Initializes the Player object"""
         super().__init__()
+
+        # Defines the player object characteristics
+        self.shape("turtle")
+        self.color("black")
+        self.penup()
+        self.setheading(90)
+        self.size = 20
+
+        # Sets the initial position
+        self.reset_position()
+
+    def reset_position(self):
+        """Resets the player position to the bottom of the screen."""
+        self.goto(0, -GRID_SIZE + ROW_HEIGHT/2)
