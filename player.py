0b30458c6fa2a04b5a630011c247d9138060728a
Bruno Silva6 <bruno_silva6@dell.com>
2025-03-19 11:17:14 -0300

    Added collision detection methond on player

--- player.py
+++ player.py
@@ -65,3 +65,12 @@ class Player(Turtle):
         """
         return self.current_lane
         
+    def collided(self, obstacle):
+        """Detects if the player has collided with an obstacle
+        Args:
+            obstacle (Obstacle): An Obstacle object for the check.
+        Returns:
+            bool: True if the player has collided with the obstacle, False otherwise
+        """
+        collision = abs(self.xcor() - obstacle.xcor()) < (self.sie/2 + obstacle.half_width)
+        return collision
